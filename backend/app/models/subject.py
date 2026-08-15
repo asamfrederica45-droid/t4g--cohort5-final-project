028a3c81f754acdf0300a0e1c7b646f89a65bbcb
asamfrederica45-droid <asamfrederica45@gmail.com>
2026-08-15 23:26:12 +0000

    A quick wrap

--- backend/app/models/subject.py
+++ backend/app/models/subject.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String,Text
 from sqlalchemy.orm import relationship
-from app.database import Base
+from app.database import Base 
 class Subject(Base): 
     __tablename__ = "subjects"
     id = Column(Integer, primary_key=True, autoincrement=True)
